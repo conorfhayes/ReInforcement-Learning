586c26274660aca7b3c42fb36e49501585591918
Conor Hayes <c.hayes2228@gmail.com>
2019-06-13 10:29:25 +0100

    Update DEED.py

--- DEED.py
+++ DEED.py
@@ -1,6 +1,6 @@
 
 # Dynamic Economic Emissions Dispatch Problem implemenation with Q-learning.
-# A V0.1 of this implementation for my MSC thesis in Data Analtics
+# A V0.1 of this implementation for my MSC thesis in Data Analytics
 # Code to be updated
 
 
