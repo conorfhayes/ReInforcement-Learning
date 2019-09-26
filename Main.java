b8d0b97cc2b09053b2348000a285b7e562a8ca7a
Conor Hayes <c.hayes2228@gmail.com>
2019-09-26 17:50:31 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class Main {
+	
+	public static int numEpisodes = 20000;
+	public static int numAgents = 9;
+	
+	public static void main(String[] args)
+	{
+		//System.out.println("Hello");
+		int inc = 1;
+		int starter = 1;
+		double[] globalCollector = {0,0,0,0};
+		ArrayList<Agent> _agentsGlobal_ = new ArrayList<Agent>();
+		
+		while (inc <= 1)
+		{	
+			Environment envGlobal = new Environment();
+			//envGlobal.setU(envGlobal.UHolder);
+			
+			while (starter <= numAgents)
+			{
+				Agent agentGlobal = envGlobal.createAgent(starter + 1);
+				agentGlobal.setU(agentGlobal.UHolder);
+				_agentsGlobal_.add(agentGlobal);
+				starter = starter + 1;
+			}
+			
+			
+			
+			System.out.println("*************** Run " + inc + " ***************");
+			
+			int j = 1;
+			while (j <= numEpisodes)
+			{
+				System.out.println("*** Episode: " + j + " ***");
+				envGlobal.timeStep(_agentsGlobal_, j, "Difference", "linear");
+				j = j + 1;
+			}
+			
+			inc = inc + 1;			
+		}		
+	}
+}
