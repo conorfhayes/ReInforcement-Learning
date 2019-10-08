7b40560dcd23c682a17401df2ae4db04f01e6474
Conor Hayes <c.hayes2228@gmail.com>
2019-10-08 18:33:34 +0100

    Update TLO_Main.java

--- TLO_Main.java
+++ TLO_Main.java
@@ -1,4 +1,10 @@
 import java.util.ArrayList;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TLO_Main {
 	
@@ -8,6 +14,7 @@ public class TLO_Main {
 	public static double totalCost;
 	public static double totalEmissions;
 	public static double totalViolations;
+	public static int numRuns = 20;
 	
 	public static ArrayList<Double> totalCostArray = new ArrayList<Double>();
 	public static ArrayList<Double> totalEmissionsArray = new ArrayList<Double>();
@@ -21,16 +28,18 @@ public class TLO_Main {
 	public static ArrayList<ArrayList<Double>> runEmissionsResults = new ArrayList<ArrayList<Double>>();
 	public static ArrayList<ArrayList<Double>> runViolationsResults = new ArrayList<ArrayList<Double>>();
 	
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
 		//System.out.println("Hello");
-		int inc = 0;
-		int starter = 1;
+		int inc = 1;
+		//int starter = 1;
 		double[] globalCollector = {0,0,0,0};
-		ArrayList<TLO_Agent> _agentsGlobal_ = new ArrayList<TLO_Agent>();
 		
-		while (inc <= 10)
+		int div = numRuns;
+		while (inc <= numRuns)
 		{	
+			int starter = 1;
+			ArrayList<TLO_Agent> _agentsGlobal_ = new ArrayList<TLO_Agent>();
 			TLO_Environment envGlobal = new TLO_Environment();
 			//envGlobal.setU(envGlobal.UHolder);
 			
@@ -53,7 +62,7 @@ public class TLO_Main {
 			
 			while (j <= numEpisodes)
 			{
-				System.out.println("*** Episode: " + j + " ***");
+				
 				timeStepVector = envGlobal.timeStep(_agentsGlobal_, j, "Global", "hypervolume");
 				
 				totalCost = timeStepVector[0];
@@ -63,10 +72,11 @@ public class TLO_Main {
 				totalCostArray.add(totalCost); totalEmissionsArray.add(totalEmissions);
 				totalViolationsArray.add(totalViolations);
 				
-				System.out.println("Total Cost: " + totalCost);
-				System.out.println("Total Emissions: " + totalEmissions);
-				System.out.println("Total Violations: " + totalViolations);				
-				System.out.println(" ");
+				//System.out.println("*** Episode: " + j + " ***");
+				//System.out.println("Total Cost: " + totalCost);
+				//System.out.println("Total Emissions: " + totalEmissions);
+				//System.out.println("Total Violations: " + totalViolations);				
+				//System.out.println(" ");
 					
 				
 				j = j + 1;
@@ -76,32 +86,66 @@ public class TLO_Main {
 			runEmissionsResults.add(totalEmissionsArray);
 			runViolationsResults.add(totalViolationsArray);
 			
-			//System.out.println(runCostResults.get(0));
-			
 			inc = inc + 1;	
 			
 		}
-			double cost = 0;
-			double emissions = 0;
-			double violations = 0;
-			for (int x = 0; x <= numEpisodes - 1; x ++)
-			{
-				for (int y = 0; y < inc; y++)
-					
-				{
-					cost = cost + runCostResults.get(y).get(x);
-					emissions = emissions + runEmissionsResults.get(y).get(x);
-					violations = violations + runViolationsResults.get(y).get(x);
-				}
+		double cost = 0;
+		double emissions = 0;
+		double violations = 0;
+		for (int x = 0; x < numEpisodes; x ++)
+		{
+			cost = 0;
+			emissions = 0;
+			violations = 0;
+			
+			for (int y = 0; y < numRuns; y++)
 				
-				cost = cost / inc; emissions = emissions / inc; violations = violations / inc;
-				cost = cost / 1000000 ; emissions = emissions / 10000000; violations = violations / 1000000; 
-				outputCostArray.add(cost); outputEmissionsArray.add(emissions); outputViolationsArray.add(violations);
+			{
+				cost = cost + runCostResults.get(y).get(x);
+				emissions = emissions + runEmissionsResults.get(y).get(x);
+				violations = violations + runViolationsResults.get(y).get(x);
 			}
 			
-			System.out.println("Output Cost Array: " + outputCostArray);
-			System.out.println("Output Emissions Array: " + outputEmissionsArray);
-			System.out.println("Output Violations Array: " + outputViolationsArray);
-				
+			cost = cost / div; emissions = emissions / div; violations = violations / div;
+			cost = cost / 1000000 ; emissions = emissions / 1000000; violations = violations / 1000000; 
+			outputCostArray.add(cost); outputEmissionsArray.add(emissions); outputViolationsArray.add(violations);
+		}
+			
+		//System.out.println("Output Cost Array: " + outputCostArray);
+		//System.out.println("Output Emissions Array: " + outputEmissionsArray);
+		//System.out.println("Output Violations Array: " + outputViolationsArray);
+		
+		FileWriter fw = new FileWriter("TLO_Cost_" + java.time.LocalDate.now() + "_" +  java.time.LocalTime.now());
+		PrintWriter pw = new PrintWriter(fw);
+		for (int e = 0; e < numEpisodes; e ++)
+		{
+			pw.println(outputCostArray.get(e).toString());
+		}
+		//br.write(outputCostArray.toString());
+		pw.flush();			
+		pw.close();
+		fw.close();
+		
+		FileWriter fw1 = new FileWriter("TLO_Emissions_" + java.time.LocalDate.now() + "_" +  java.time.LocalTime.now());
+		PrintWriter pw1 = new PrintWriter(fw1);
+		for (int e = 0; e < numEpisodes; e ++)
+		{
+			pw1.println(outputEmissionsArray.get(e).toString());
+		}
+		//br.write(outputCostArray.toString());
+		pw1.flush();			
+		pw1.close();
+		fw1.close();
+		
+		FileWriter fw2 = new FileWriter("TLO_Violations_" + java.time.LocalDate.now() + "_" +  java.time.LocalTime.now());
+		PrintWriter pw2 = new PrintWriter(fw2);
+		for (int e = 0; e < numEpisodes; e ++)
+		{
+			pw2.println(outputEmissionsArray.get(e).toString());
+		}
+		//br.write(outputCostArray.toString());
+		pw2.flush();			
+		pw2.close();
+		fw2.close();
 	}
 }
