78872007a679af0db96975e3e6a31271782db767
Conor Hayes <c.hayes2228@gmail.com>
2020-04-01 21:51:04 +0100

    Create commands_deepseatreasure.sh

---
+++ Python/Distributional RL/Distributional RL Paper Code/Deep Sea Treasure/commands_deepseatreasure.sh
@@ -0,0 +1,1 @@
+python3 Node_DistRL.py --name deepseatreasure --env DeepSeaTreasure-v0 --episodes 10000 --avg 10 --hidden 50 --lr 0.001 --ret forward --extra-state none --utility "(r1 * 1) + (r2 * 1)" > /dev/null
