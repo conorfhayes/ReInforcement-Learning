6a9ee3fdcce3649e547afc49f813c060b9e07ebc
Conor Hayes <c.hayes2228@gmail.com>
2020-04-03 12:14:49 +0100

    Update commands_deepseatreasure_danger.sh

--- Python/Distributional RL/Distributional RL Paper Code/Deep Sea Treasure (Danger)/commands_deepseatreasure_danger.sh
+++ Python/Distributional RL/Distributional RL Paper Code/Deep Sea Treasure (Danger)/commands_deepseatreasure_danger.sh
@@ -1,1 +1,1 @@
-python3 Node_DistRL.py --name deepseatreasure_danger --env DeepSeaTreasure-v0 --episodes 10000 --avg 10 --hidden 50 --lr 0.001 --ret forward --extra-state none --utility "(r1 * 1) + (r2 * 1)" > /dev/null
+python3 Node_DistRL.py --name deepseatreasure_danger --env DeepSeaTreasureDanger-v0 --episodes 10000 --avg 10 --hidden 50 --lr 0.001 --ret forward --extra-state none --utility "(r1 * 1) + (r2 * 1)" > /dev/null
