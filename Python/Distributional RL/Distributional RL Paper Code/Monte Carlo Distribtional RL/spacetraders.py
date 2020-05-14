e28448625dde9ac3242e3d8844314e9179ed7211
Conor Hayes <c.hayes2228@gmail.com>
2020-05-14 08:59:46 +0100

    Update spacetraders.py

--- Python/Distributional RL/Distributional RL Paper Code/Monte Carlo Distribtional RL/spacetraders.py
+++ Python/Distributional RL/Distributional RL Paper Code/Monte Carlo Distribtional RL/spacetraders.py
@@ -7,12 +7,12 @@ from gym.utils import seeding
 import random
 import sys
 
+
 class SpaceTraders(gym.Env):
-    
 
     def __init__(self):
 
-        self.action_space = spaces.Discrete(3)         
+        self.action_space = spaces.Discrete(3)
         self.num_rewards = 2
 
         self.reset()
@@ -21,86 +21,92 @@ class SpaceTraders(gym.Env):
 
         self.state = 0
 
-        return self.state    
-
+        return self.state
 
     def step(self, state, action):
 
         rewards = np.zeros((2,))
+        _random_ = random.random()
         done = False
 
         if action > 2:
-        	return -1000000, [-1000000, -1000000], True
+            return -1000000, [-1000000, -1000000], True, {}
 
-    	# Timestep 1
-        # Planet A -> Planet B
+        # Timestep 1
+        # Planet A -> Planet B        
         if state == 0 and action == 0:
-        	state = 1
-        	rewards = [0, -12]
+            next_state = 1
+            rewards = [0, -12]
+            done = False
+    
+            return next_state, np.array(rewards), done, {}
+    
+        if state == 0 and action == 1 and _random_ <= 0.9:
+    
+            next_state = 1
+            rewards = [0, -6]
+            done = False
+    
+            return next_state, np.array(rewards), done, {}
+    
+        elif state == 0 and action == 1 and _random_ > 0.9:
+    
+            next_state = 1
+            rewards = [0, -1]
+            done = True
+    
+            return next_state, np.array(rewards), done, {}
+    
+        if state == 0 and action == 2 and _random_ <= 0.85:
+            next_state = 1
+            rewards = [0, 0]
+            done = False
+    
+            return next_state, np.array(rewards), done, {}
+    
+        elif state == 0 and action == 2 and _random_ > 0.85:
+            next_state = 1
+            rewards = [0, 0]
+            done = True
+    
+            return next_state, np.array(rewards), done, {}
+    
+        # Timestep 2
+        # Planet B -> Planet A
+        if state == 1 and action == 0:
+            next_state = 2
+            rewards = [1, -10]
+            done = True
+            return next_state, np.array(rewards), done, {}
+    
+        if state == 1 and action == 1 and _random_ <= 0.9:
+    
+            next_state = 2
+            rewards = [1, -8]
+            done = True
+    
+            return next_state, np.array(rewards), done, {}
+    
+        elif state == 1 and action == 1 and _random_ > 0.9:
+            next_state = 2
+            rewards = [0, -7]
+            done = True
+    
+            return next_state, np.array(rewards), done, {}
+    
+        if state == 1 and action == 2 and _random_ <= 0.85:
+            next_state = 2
+            rewards = [1, 0]
+            state = 0
+            done = True
+    
+            return next_state, np.array(rewards), done, {}
+    
+        elif state == 1 and action == 2 and _random_ > 0.85:
+            next_state = 2
+            rewards = [0, 0]
+            done = True
+    
+            return next_state, np.array(rewards), done, {}
 
-        	return state, rewards, done, {}
-
-    	if state == 0 and action == 1 and random.random() <= 0.9:
-    		state = 1
-    		rewards = [0, -6]
-
-    		return state, rewards, done, {}
-
-		elif state == 0 and action == 1 and random.random() > 0.9:
-			rewards = [0, -1]
-			done == True
-
-			return state, rewards, done, {}
-
-		if state == 0 and action == 2 and random.random() <= 0.85:
-			rewards = [0,0]
-			state = 1
-
-			return state, rewards, done, {}
-
-		elif state == 0 and action == 2 and random.random() > 0.85:
-			rewards = [0,0]
-			done == True
-
-			return state, rewards, done, {}
-
-
-
-		#Timestep 2
-		#Planet B -> Planet A
-		if state == 1 and action == 0:
-			state = 0
-        	rewards = [1, -10]
-        	done == True
-
-        	return state, rewards, done, {}   
-
-    	if state == 1 and action == 1 and random.random() <= 0.9:
-    		state = 0
-    		rewards = [1, -8]
-    		done == True
-
-    		return state, rewards, done, {}
-
-		elif state == 1 and action == 1 and random.random() > 0.9:
-			rewards = [0, -7]
-			done == True
-
-			return state, rewards, done, {}
-
-		if state == 1 and action == 2 and random.random() <= 0.85:
-			rewards = [1,0]
-			state = 0
-			done = True
-
-			return state, rewards, done, {}
-
-		elif state == 0 and action == 2 and random.random() > 0.85:
-			rewards = [0,0]
-			done == True
-
-			return state, rewards, done, {}   
-        
-       
-
-        return state, rewards, done, {}
+      
